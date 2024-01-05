11e875b504b092bf7d25ad4c5dd4a0a2ab6922a7
Scott W <scottaza@gmail.com>
2024-01-05 16:54:34 -0700

    Add Book listing app

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"), #TODO: Create index() view
-    path("posts", views.get_posts_list, name="posts-list"), #TODO: Cteate get_posts_list() view
-    path("posts/<post_id>", views.get_post, name="post-detail") #TODO: Create get_post() view
+    path("", views.index, name="index"), 
+    path("posts", views.get_posts_list, name="posts-list"), 
+    path("posts/<post_id>", views.get_post, name="post-detail") 
 ]
--- conglomo/urls.py
+++ conglomo/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('books/', include("book_outlet.urls")),
     path('admin/', admin.site.urls),
     path('', include("blog.urls"))
+    
 ]
